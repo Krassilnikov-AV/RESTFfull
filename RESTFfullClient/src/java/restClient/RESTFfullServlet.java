36c361d88309a7736dbb3a8a5cf4a2e196545ce7
Aleks <Aleks@AleKs>
2020-06-01 21:11:46 +0300

    Initial commit

--- RESTFfullClient/src/java/restClient/RESTFfullServlet.java
+++ RESTFfullClient/src/java/restClient/RESTFfullServlet.java
@@ -30,29 +30,22 @@ public class RESTFfullServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
 
         String tdirect = request.getParameter("direct");
-        String tfolder = request.getParameter("folder");
+        String tfile = request.getParameter("file");
 
-        boolean direct = request.getParameter("direct") != null;
-        boolean sfolder = request.getParameter("sfolder") != null;
+        boolean sdirect = request.getParameter("direct") != null;
+        boolean sfile = request.getParameter("sfile") != null;
 
         RESTFfullClient rst2 = new RESTFfullClient();
 
         String result = null;
-        String resultdir;
+//  String resultdir;
         try (PrintWriter out = response.getWriter()) {
-            if (direct) {
-                String resCon = rst2.altGetContent(tdirect);
-                result = resCon;              
+            if (sdirect) {
+// вывод 
+                result = rst2.getContent(tdirect);                            
             }
-            if (sfolder) {
-                File f = new File(result);
-                  resultdir = result;
-// boolean isDirectory() - Проверяет, является ли файл, обозначенный этим абстрактным путем, каталогом.
-                if(f.isDirectory()) {
-                  result = rst2.findFile(resultdir, tfolder);
-                }
-                result = rst2.altFindFile(resultdir, tfolder);
-                //               rst2.close();
+            if (sfile) {
+                result = rst2.altFindFile(tdirect, tfile);              
             }
             request.setAttribute("result", "\n" + result.toString());
             request.getRequestDispatcher("index.jsp").forward(request, response);
